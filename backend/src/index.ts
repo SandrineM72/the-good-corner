c5b50286b38763eef925c38d001c07e8e5c8a3f0
SandrineM72 <sandrine.maingourd.pro72@gmail.com>
2025-11-27 18:09:39 +0100

    Site petites annonces - premier commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,131 +4,200 @@ import sqlite from "sqlite3";
 const app = express();
 app.use(express.json());
 const port = 3000;
-const db = new sqlite.Database('db.sqlite');
+const db = new sqlite.Database('newdb.sqlite');
 
 
-// let ads = [
-//   {
-//     id: 1,
-//     title: "Bike to sell",
-//     description:
-//       "My bike is blue, working fine. I'm selling it because I've got a new one",
-//     owner: "bike.seller@gmail.com",
-//     price: 100,
-//     picture:
-//       "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
-//     location: "Paris",
-//     createdAt: "2023-09-05T10:13:14.755Z",
-//   },
-//   {
-//     id: 2,
-//     title: "Car to sell",
-//     description:
-//       "My car is blue, working fine. I'm selling it because I've got a new one",
-//     owner: "car.seller@gmail.com",
-//     price: 10000,
-//     picture:
-//       "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
-//     location: "Paris",
-//     createdAt: "2023-10-05T10:14:15.922Z",
-//   },
-// ];
+interface Ad {
+  id: number
+  title: string
+  price: number
+}
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+/**** REQUETES GET ****/
 
-// app.get("/ads", (req, res) => {
-//   res.send(ads);
-// });
-
-app.get ("/ads", (req, res) => {
-  db.all("SELECT * FROM ads", (err, rows) => {
-      if (err) {
-        return res.send(500)
-      }
-      console.log("ok");
-      res.send(rows);
-    },
-
-  )});
-
-
-// app.post("/ads", (req, res) => {
-//   ads.push(req.body);
-//   res.send("Request received, check the backend terminal");
-// });
-
-app.post("/ads", (req, res) => {
-  db.run("INSERT INTO ads (title,price) VALUES ($title, $price)", {
-    $title: req.body.title,
-    $price: req.body.price
-  }, (err) => {
+app.get('/ads', (req, res) => {
+  db.all('SELECT * FROM ads', (err, ads) => {
     if (err) {
-      return res.send(500)
+      console.error(err)
+      return res.sendStatus(500)
     }
-      console.log("ok");
-      res.send("OK");
+    res.send(ads)
   })
 })
 
-// app.delete('/ads/:id', (req, res) => {
-//   const adId = parseInt(req.params.id)
-//   const adToDelete = ads.find(ad => ad.id === adId)
-//   if (!adToDelete) return res.sendStatus(404)
-//   ads = ads.filter(ad => ad.id !== adToDelete.id)
-//   res.send("ad deleted");
-// })
-// app.delete('/ads/:id', (req, res) => {
-
-
-app.delete("/ads/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  db.run("DELETE FROM ads WHERE id = $id",
-    {
-      $id: id
+app.get('/adscategory', (req, res) => {
+  db.all('SELECT * FROM adscategory', (err, ads) => {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
     }
-    , (err) => {
-      if (err) {
-      console.log(err);
-      return res.sendStatus(500);
-    }
+    res.send(ads)
+  })
+})
 
-      res.send("OK");
+app.get('/tag', (req, res) => {
+  db.all('SELECT * FROM tag', (err, ads) => {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+    res.send(ads)
   })
 })
 
 
-// app.patch('/ads/:id', (req, res) => {
-//   const adId = parseInt(req.params.id)
-//   const adToUpdate = ads.find(ad => ad.id === adId)
-//   if (!adToUpdate) return res.sendStatus(404)
-//   const newProps = req.body
-//   Object.assign(adToUpdate, newProps)
-//   res.send("ad updated");
-// })
+/**** REQUETES POST ****/
 
-app.patch("/ads/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  db.run("UPDATE FROM ads (id) VALUE ($id)",
-    {
-      $id: id
+app.post('/ads', (req, res) => {
+  db.run('INSERT INTO ads (title, price, picture, city, description, category_id) VALUES ($title, $price, $picture, $city, $description, $category_id)', {
+
+    $title: req.body.title,
+    $price: req.body.price,
+    $picture: req.body.picture,
+    $city: req.body.city,
+    $description: req.body.description,
+    $category_id: req.body.category_id
+
+  }, function (err) {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
     }
-        , (err) => {
-      if (err) {
-      console.log(err);
-      return res.sendStatus(500);
-    }
-
-      res.send("OK");
-  }
-
-  )
+    res.send({ ...req.body, id: this.lastID })
+  })
 })
 
+app.post('/adscategory', (req, res) => {
+  db.run('INSERT INTO adscategory (categoryname) VALUES ($categoryname)', {
+    
+    $categoryname: req.body.categoryname
+
+  }, function (err) {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+    res.send({ ...req.body, id: this.lastID })
+  })
+})
+
+app.post('/tag', (req, res) => {
+  db.run('INSERT INTO tag (name) VALUES ($name)', {
+    
+    $name: req.body.name
+
+  }, function (err) {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+    res.send({ ...req.body, id: this.lastID })
+  })
+})
+
+
+
+/**** REQUETES DELETE POUR SUPPRIMER 1 ENREGISTREMENT ****/
+
+app.delete('/ads/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10)
+  db.get('SELECT * FROM ads WHERE id = $id', {
+    $id: id
+  }, (err, adToDelete: Ad) => {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+    if (!adToDelete) return res.sendStatus(404)
+    db.run('DELETE FROM ads WHERE id = $id', {
+      $id: id
+    }, (err) => {
+      if (err) {
+        console.error(err)
+        return res.sendStatus(500)
+      }
+      res.send('ad deleted !')
+    })
+  })
+})
+
+app.delete('/adscategory/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10)
+  db.get('SELECT * FROM adscategory WHERE id = $id', {
+    $id: id
+  }, (err, adToDelete: Ad) => {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+    if (!adToDelete) return res.sendStatus(404)
+    db.run('DELETE FROM adscategory WHERE id = $id', {
+      $id: id
+    }, (err) => {
+      if (err) {
+        console.error(err)
+        return res.sendStatus(500)
+      }
+      res.send('category deleted !')
+    })
+  })
+})
+
+app.delete('/tag/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10)
+  db.get('SELECT * FROM tag WHERE id = $id', {
+    $id: id
+  }, (err, adToDelete: Ad) => {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+    if (!adToDelete) return res.sendStatus(404)
+    db.run('DELETE FROM tag WHERE id = $id', {
+      $id: id
+    }, (err) => {
+      if (err) {
+        console.error(err)
+        return res.sendStatus(500)
+      }
+      res.send('tag deleted !')
+    })
+  })
+})
+
+
+
+/**** REQUETES PATCH POUR MODIFIER 1 ENREGISTREMENT ****/
+
+/**** A TERMINER ****/
+
+
+app.patch('/ads/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10)
+
+  db.get('SELECT * FROM ads WHERE id = $id', {
+    $id: id
+  }, (err, adToUpdate: Ad) => {
+    if (err) {
+      console.error(err)
+      return res.sendStatus(500)
+    }
+    if (!adToUpdate) return res.sendStatus(404)
+    db.run('UPDATE ads SET title=$title, price=$price WHERE id = $id', {
+      $id: id,
+      $title: req.body.title || adToUpdate.title,
+      $price: req.body.price || adToUpdate.price
+    }, (err) => {
+      if (err) {
+        console.error(err)
+        return res.sendStatus(500)
+      }
+      res.send('ad updated !')
+    })
+  })
+})
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
